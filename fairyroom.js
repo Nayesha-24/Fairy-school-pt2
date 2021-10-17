a18a9349f14f4c2f1461b5483598176c5197d5f3
Nayesha-24 <84524247+Nayesha-24@users.noreply.github.com>
2021-10-17 11:47:27 +0530

    Add files via upload

---
+++ fairyroom.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCxQsaxcl4YcYfGgTvRxus7F1rNG0Prkms",
+    authDomain: "project-st-fairy-school.firebaseapp.com",
+    databaseURL: "https://project-st-fairy-school-default-rtdb.firebaseio.com",
+    projectId: "project-st-fairy-school",
+    storageBucket: "project-st-fairy-school.appspot.com",
+    messagingSenderId: "569657487046",
+    appId: "1:569657487046:web:b3511298468a721e5331c7",
+    measurementId: "G-6QP6916B4R"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
